50b7f188096a5e2a2e7d7eb396877624c9df2070
Imperi13 <imperi.night@gmail.com>
2020-03-08 14:43:22 +0900

    add counter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <counter />
   </div>
 </template>
 
 <script>
+import Counter from "./Counter.vue";
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  components: {
+    Counter
   }
 };
 </script>
